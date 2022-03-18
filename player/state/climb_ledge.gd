c2b8a64306d2f525c7132ecaea5f1c335ebbf11f
streq <strequena@gmail.com>
2022-03-18 08:54:38 -0300

    small fix on ledgeclimbing

--- player/state/climb_ledge.gd
+++ player/state/climb_ledge.gd
@@ -4,7 +4,7 @@ class_name ClimbLedgeState
 var right_ledge : CollisionShape2D
 var left_ledge : CollisionShape2D
 var done = false
-onready var tween := $Tween 
+onready var tween : Tween = $Tween 
 func enter(args):
 	right_ledge = owner.get_node("right_ledge_grab")
 	left_ledge = owner.get_node("left_ledge_grab")
@@ -20,19 +20,19 @@ func update(delta:float):
 	var ledge = right_ledge if !right_ledge.disabled else left_ledge
 	
 	var final_pos = ledge.global_position - Vector2(-ledge_dir*5.0, 12.0)
-	tween.interpolate_property(p,"global_position",p.global_position, Vector2(p.global_position.x, final_pos.y), 0.1,Tween.TRANS_LINEAR)
-	tween.interpolate_property(owner.sprite,"position",owner.sprite.position, Vector2(0.0, 4.0),0.1,Tween.TRANS_LINEAR)
+	tween.interpolate_property(p,"global_position",p.global_position, Vector2(p.global_position.x, final_pos.y), 0.2,Tween.TRANS_LINEAR)
+	tween.interpolate_property(owner.sprite,"position",owner.sprite.position, Vector2(0.0, 4.0),0.2,Tween.TRANS_LINEAR)
 	
 	
 	tween.start()
 	yield(tween,"tween_completed")
-	
-	tween.interpolate_property(p,"global_position",p.global_position, Vector2(final_pos.x, p.global_position.y), 0.1, Tween.TRANS_LINEAR)
-	
-	yield(tween,"tween_completed")
-	
+	yield(get_tree(),"idle_frame")#so "tween_completed" doesn't match both yields
 	owner.animation.play("air")
 	owner.sprite.rotation_degrees = 90*owner.facing_dir
+	tween.interpolate_property(p,"global_position",p.global_position, Vector2(final_pos.x, p.global_position.y), 0.1, Tween.TRANS_LINEAR)
+	tween.start()
+	yield(tween,"tween_completed")
+	
 	
 	yield(get_tree().create_timer(0.1), "timeout")
 	p.sprite.rotation_degrees = 0
