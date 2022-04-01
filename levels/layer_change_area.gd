a0ea0f075d47a315bea3ab43dd10269986b1fd24
streq <strequena@gmail.com>
2022-04-01 13:19:51 -0300

    layer change area signal connection bug fix

--- levels/layer_change_area.gd
+++ levels/layer_change_area.gd
@@ -3,10 +3,14 @@ export var direction := 1
 
 
 func _on_body_entered(body:PhysicsBody2D):
-	var root = get_tree().get_nodes_in_group("level_root")[0]
-	if !body.is_connected("interact", root, "change_layer"):
-		body.connect("interact", root, "change_layer", [direction])
+#	print(body.name, " entered ", name)
+	if !body.is_connected("interact", self, "change_layer"):
+		body.connect("interact", self, "change_layer")
 func _on_body_exited(body:PhysicsBody2D):
+#	print(body.name, " exited ", name)
+	if body.is_connected("interact", self, "change_layer"):
+		body.disconnect("interact", self, "change_layer")
+
+func change_layer():
 	var root = get_tree().get_nodes_in_group("level_root")[0]
-	if body.is_connected("interact", root, "change_layer"):
-		body.disconnect("interact", root, "change_layer")
+	root.change_layer(direction)
